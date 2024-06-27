73adf1fb7c467ccff1e349f9aaf89cb76ecf103a
haqqibrahim <enessyibrahim@gmail.com>
2024-06-27 09:43:19 +0100

    AI library fixed 2

--- pages/1_idea.py
+++ pages/1_idea.py
@@ -1,10 +1,5 @@
 import streamlit as st
 import shelve
-from phi.assistant import Assistant
-from phi.llm.groq import Groq
-from phi.tools.arxiv_toolkit import ArxivToolkit
-from phi.tools.duckduckgo import DuckDuckGo
-from phi.tools.exa import ExaTools
 from ai.idea import ai
 
 st.subheader("Generate ideas/topic for your Project")
@@ -19,25 +14,7 @@ exa_api_key = st.secrets["EXA_API_KEY"]
 # st.markdown("##### :blue_heart: built by [phidata](https://github.com/phidatahq/phidata)")
 
   
-def ai_ai(prompt):
-    assistant = Assistant(
-    llm=Groq(model="llama3-70b-8192",api_key=groq_api_key),
-    description="You World Class researcher assigned a very important task. Given a subject or context or topic, your job is to create two(2) spinoff ideas based on the subject or context or topic.",
-    instructions=[
-        "Getting information of what research has been done based on the topic/context/subject",
-        "Provide existing papers that relate to the topic/context/subjects, maximum of 8",
-        "Come up with 2 spinoff ideas to improve upon the past or exisitng research",
-        "generate description for the 2 spinoff project in a bullet point format",
-        "suggest papers to read based on the 2 spinoff"
-    ],
-    tools=[DuckDuckGo(), ExaTools(api_key=exa_api_key)]
-    # debug_mode=True,
-    )
-    
-    response = assistant.run(prompt, stream=False)
-    return response
 
-    
 
 # Load chat history from shelve file
 def load_chat_history():
